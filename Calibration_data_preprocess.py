4e21883991ad174355ccbc88487be3b0544688ae
María Fernanda Folch <mfernandafolch@uc.cl>
2026-01-09 09:45:17 -0300

    cambio del nombre de FILE_PATH del archivo Calibration_data_preprocess.py para poder correrlo en el PC.

--- Calibration_data_preprocess.py
+++ Calibration_data_preprocess.py
@@ -14,7 +14,8 @@ from math import ceil
 from sklearn.isotonic import IsotonicRegression
 
 # ===================== CONFIG =====================
-FILE_PATH = r"C:\Users\ctorrealba\OneDrive - Viña Concha y Toro S.A\Documentos\Proyectos I+D\PI-4497\Resultados\2025\SB_Calibration_2025\Procesos_I+D_2025_3.xlsx"   # <-- EDITA
+# FILE_PATH = r"C:\Users\ctorrealba\OneDrive - Viña Concha y Toro S.A\Documentos\Proyectos I+D\PI-4497\Resultados\2025\SB_Calibration_2025\Procesos_I+D_2025_3.xlsx"   # <-- EDITA
+FILE_PATH = r"C:\Users\MARIA\Documents\SB_Calibration_2025\Procesos_I+D_2025_3.xlsx"
 SHEET_BDD = "BDD_Maestra"
 
 # Carpeta con planillas de temperatura "Data <ID>.xlsx"
